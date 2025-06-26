19f706c69eaca13c0ae67cffe7f1fc157d3a6ff1
ProNinjaDev <pronin-04@bk.ru>
2025-06-26 11:07:49 +0300

    Collect all services in Program.cs. Create a default manager

--- ConsoleTaskManager/Program.cs
+++ ConsoleTaskManager/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ConsoleTaskManager.Models;
+using ConsoleTaskManager.Services;
+using ConsoleTaskManager.Services.Interfaces;
+using ConsoleTaskManager.Storage;
+using ConsoleTaskManager.Storage.Interfaces;
+
+IDataStorage dataStorage = new JsonDataStorage("users.json", "tasks.json");
+
+IAuthService authService = new AuthService(dataStorage);
+IUserService userService = new UserService(dataStorage);
+ITaskService taskService = new TaskService(dataStorage);
+
+
+await InitializeApplication();
+
+// TODO: добавить меню
+
+async Task InitializeApplication() 
+{
+    var existingUsers = await dataStorage.LoadUsersAsync();
+
+    if (!existingUsers.Any()) 
+    {
+        Console.WriteLine("User base is empty");
+        await userService.CreateUserAsync("Admin", "Adminpassword555", UserRole.Manager);
+        Console.WriteLine("Managing user is created by default");
+    }
+    else 
+    {
+        Console.WriteLine("User base has been discovered");
+    }
+}
